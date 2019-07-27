f8e507db718814aaf6b76040d5f6c30c9c13abee
Darayavaus <darayavaus.chern@yandex.ru>
2019-07-27 18:02:24 +0300

    petya user test data

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,18 @@
 from flask import render_template, redirect, flash, jsonify
 from . import app
 
+import json
+
+MAPPING = {}
+with open('db/mapping.json', 'r') as f:
+    MAPPING = json.load(f)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open('db/petya.json', 'r') as f:
+        petya = json.load(f)
+    attention = []
+    for a in petya['attention']:
+        attention.append(MAPPING['2'][str(a.split(';')[-1])])
+    petya['attention_strings'] = attention
+    return render_template('index.html', petya=petya)
