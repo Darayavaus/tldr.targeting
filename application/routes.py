8a0a64485759700edbaf3d521407aacfe5c99ca8
Darayavaus <darayavaus.chern@yandex.ru>
2019-07-27 19:44:41 +0300

    api/get_attention method

--- application/routes.py
+++ application/routes.py
@@ -7,6 +7,10 @@ MAPPING = {}
 with open('db/mapping.json', 'r') as f:
     MAPPING = json.load(f)
 
+MATERIALS = []
+with open('db/materials.json', 'r') as f:
+    MATERIALS = json.load(f)
+
 @app.route('/')
 def index():
     with open('db/petya.json', 'r') as f:
@@ -22,3 +26,23 @@ def index():
         favorite.append(MAPPING[str(len(tmp)-1)][str(tmp[-1])])
     petya['favorite_strings'] = favorite
     return render_template('index.html', petya=petya)
+
+
+@app.route('/api/get_favorite')
+def get_favorite():
+    return jsonify({
+        'favorite': []
+    })
+
+@app.route('/api/get_attention')
+def get_attention():
+    with open('db/petya.json', 'r') as f:
+        petya = json.load(f)
+    attention = []
+    for a in petya['attention']:
+        for mat in MATERIALS:
+            if a in mat['kes']:
+                attention.append({mat['name']: {"type": mat['type']}})
+    return jsonify({
+        'attention': attention
+    })
